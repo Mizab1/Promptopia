323922f75f27dde390f448979527ad10a808e551
Mizab <mizabmansari@gmail.com>
2023-07-17 22:06:56 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "Promptopia",
+  description:
+    "Promptopia is a simple, intuitive, and easy-to-use prompt system for React.",
+  author: "Mizab",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
