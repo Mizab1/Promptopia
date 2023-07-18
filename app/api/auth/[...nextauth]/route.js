7d92ff7c58c70ac520c883047db80145d5df3674
Mizab <mizabmansari@gmail.com>
2023-07-18 23:14:30 +0530

    Bug Fixes 🪲

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -13,7 +13,7 @@ const handler = NextAuth({
   async session({ session }) {
     const sessionUser = await User.findOne({ email: session.user.email });
     session.user.id = sessionUser._id.toString();
-    return true;
+    return session;
   },
   async signIn({ profile }) {
     try {
@@ -26,6 +26,7 @@ const handler = NextAuth({
           image: profile.picture,
         });
       }
+      return true;
     } catch (error) {
       console.log(error);
       return false;
