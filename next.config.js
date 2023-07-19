aabecdcf854cdb0ffa0eece335269d2c9f956f52
Mizab <mizabmansari@gmail.com>
2023-07-19 22:56:52 +0530

    Changed Configs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
   images: {
     domains: ["lh3.googleusercontent.com"],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    // config.resolve.fallback = {
+    //   "mongodb-client-encryption": false,
+    //   aws4: false,
+    //   "supports-color": false,
+    //   kerberos: false,
+    //   "@mongodb-js/zstd": false,
+    //   "@aws-sdk/credential-providers": false,
+    //   snappy: false,
+    // };
+
+    return config;
   },
 };
 
